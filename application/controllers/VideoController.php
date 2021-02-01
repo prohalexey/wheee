362eaeea4eeda61d7aad39aa194d939641696b52
Prokhorov Alexey <proh.alexey@gmail.com>
2021-02-01 21:23:53 +0300

    Fix exception logic

--- application/controllers/VideoController.php
+++ application/controllers/VideoController.php
@@ -5,12 +5,14 @@ namespace application\controllers;
 use application\infrastructure\videos\policy\ThumbnailPathPolicy;
 use application\infrastructure\videos\repository\VideoRepository;
 
+use Domain\Videos\Exception\EntityNotFoundException;
 use Domain\Videos\UseCase\UpdateVideoViewCounter;
 
 use Yii;
 use yii\log\Logger;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
+use yii\web\ServerErrorHttpException;
 
 class VideoController extends Controller
 {
@@ -31,10 +33,12 @@ class VideoController extends Controller
 
             // Todo: Load from DI container like ThumbnailPathPolicyContract
             $thumbnail = (new ThumbnailPathPolicy())->getThumbnail($video);
+        } catch (EntityNotFoundException $e) {
+            throw new NotFoundHttpException('Video not found');
         } catch (\Throwable $e) {
             Yii::getLogger()->log($e->getMessage(), Logger::LEVEL_ERROR);
 
-            throw new NotFoundHttpException('Video not found');
+            throw new ServerErrorHttpException('Video not found');
         }
 
         // We can skip the error even if there was an error here,
